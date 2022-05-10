7f2b931d43f314f0d7b9784f2e77eb097c7aec7d
kgroves-netbook <kgroves@besbox.com>
2022-05-10 21:31:36 +0100

    Fix

--- pre.sh
+++ pre.sh
@@ -2,7 +2,7 @@
 
 for f in constants.asm IOSerial.asm MINT.asm ram.asm ; do
 
-cat $f | tr A-Z a-z | sed 's/\.org /org /g' | sed 's/\.equ/equ/g' | sed 's/ +equ/: equ/g' | sed 's/\.if /if /g'| sed 's/\.else /else /g' | sed 's/\.endif/endif /g' >a_$f
+cat $f | tr A-Z a-z | sed 's/\.org /org /g' | sed 's/\.equ/equ/g' | sed 's/ *equ/: equ/g' | sed 's/\([a-z]\) *equ/\1: equ/g'| sed 's/\.if /if /g'| sed 's/\.else /else /g' | sed 's/\.endif/endif /g' >a_$f
 done 
 z80asm z80homebrew.z80
 
